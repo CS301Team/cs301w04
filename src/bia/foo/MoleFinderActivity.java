212c63f901814cbded8425141404f1bf03f5ba06
abudac <abudac@ualberta.ca>
2012-02-09 14:38:29 -0700

    ewrfal;

---
+++ src/bia/foo/MoleFinderActivity.java
@@ -0,0 +1,13 @@
+package bia.foo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoleFinderActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
