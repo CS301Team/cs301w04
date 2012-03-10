9c8ebea53a2ec4e5f3538b7129f4a5880cf4727f
christianjukna <jukna@ualberta.ca>
2012-03-10 13:05:33 -0700

    updated the activitys to work with proper view

--- src/bia/foo/PhotoLayoutView.java
+++ src/bia/foo/PhotoLayoutView.java
@@ -7,6 +7,6 @@ public class PhotoLayoutView extends Activity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main); //insert proper layout here
+        setContentView(R.layout.photogridview);
     }
 }
