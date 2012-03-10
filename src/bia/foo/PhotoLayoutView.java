3dbed96e22c9be33658a31f8742f04a7b91973b9
christianjukna <jukna@ualberta.ca>
2012-03-10 12:43:30 -0700

    Added the views as per the user interface diagram

---
+++ src/bia/foo/PhotoLayoutView.java
@@ -0,0 +1,12 @@
+package bia.foo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhotoLayoutView extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main); //insert proper layout here
+    }
+}
