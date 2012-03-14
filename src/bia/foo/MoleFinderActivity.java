e9ea13118f204958d062149fca666b11bfb9a266
christianjukna <jukna@ualberta.ca>
2012-03-14 13:43:13 -0600

    MoleFinderActivity fixed dialogue prompt

--- src/bia/foo/MoleFinderActivity.java
+++ src/bia/foo/MoleFinderActivity.java
@@ -2,19 +2,26 @@ package bia.foo;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 //this is the initial view which shows groups of photos
 public class MoleFinderActivity extends Activity {
 	private Button addFolderButton;
 	private Button deleteFolderButton;
-	@SuppressWarnings("unused")
 	private Editable folderName;
+	private EditText input;
+	
+	static final int DIALOG_NEW_FOLDER_ID = 0;
+	static final int DIALOG_DELETE_FOLDER_ID = 1;
 	
     /** Called when the activity is first created. */
     @Override
@@ -22,59 +29,45 @@ public class MoleFinderActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        final EditText input = new EditText(MoleFinderActivity.this);
+    //    input = new EditText(MoleFinderActivity.this);
         addFolderButton = (Button) findViewById(R.id.add_group);
         deleteFolderButton = (Button) findViewById(R.id.delete_group);
         
-        //creation of the dialog box confirming item deletion
-        final AlertDialog.Builder addDialog = new AlertDialog.Builder(MoleFinderActivity.this);
-        final AlertDialog.Builder deleteDialog = new AlertDialog.Builder(MoleFinderActivity.this);
-        // Setting Dialog Title
-        addDialog.setTitle("Adding a new folder...");
-        deleteDialog.setTitle("Confirm Delete...");
-        // Setting Dialog Message
-        addDialog.setMessage("Please specify the folder name to add.");
-        deleteDialog.setMessage("Are you sure you want to delete this group?");
-        // Setting the view the new folder dialog will use
-        addDialog.setView(input);
-        
-     // Setting Positive "Yes" Button
-        addDialog.setPositiveButton("Add folder", new DialogInterface.OnClickListener() {
-        	public void onClick(DialogInterface dialog,int which) {
-        		//actions to complete when clicking yes
-        		folderName = input.getText();
-        	}
-        });
-
-        // Setting Negative "NO" Button
-        addDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-        	public void onClick(DialogInterface dialog, int which) {
-        		//actions to complete when clicking cancel
-        		dialog.cancel();
-        	}
-        });
-
-        // Setting Positive "Yes" Button
-        deleteDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-        	public void onClick(DialogInterface dialog,int which) {
-        		//actions to complete when clicking yes
-        	}
-        });
-
-        // Setting Negative "NO" Button
-        deleteDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-        	public void onClick(DialogInterface dialog, int which) {
-        		//actions to complete when clicking cancel
-        		dialog.cancel();
-        	}
-        });
-
-        
+//        //creation of the dialog box confirming item deletion
+//        final AlertDialog.Builder addDialog = new AlertDialog.Builder(MoleFinderActivity.this);
+//        final AlertDialog.Builder deleteDialog = new AlertDialog.Builder(MoleFinderActivity.this);
+//        // Setting Dialog Title
+//        addDialog.setTitle("Adding a new folder...");
+//        deleteDialog.setTitle("Confirm Delete...");
+//        // Setting Dialog Message
+//        addDialog.setMessage("Please specify the folder name to add.");
+//        deleteDialog.setMessage("Are you sure you want to delete this group?");
+//        // Setting the view the new folder dialog will use
+//        addDialog.setView(input);
+//
+//
+//        // Setting Positive "Yes" Button
+//        deleteDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//        	public void onClick(DialogInterface dialog,int which) {
+//        		//actions to complete when clicking yes
+//        		dialog.cancel();
+//        	}
+//        });
+//
+//        // Setting Negative "NO" Button
+//        deleteDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//        	public void onClick(DialogInterface dialog, int which) {
+//        		//actions to complete when clicking cancel
+//        		dialog.cancel();
+//        	}
+//        });
+//
+//
         addFolderButton.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View v) {
 //        		if () {
 //        			// brings up the dialog box to confirm deletion
-        			addDialog.show();
+        			showDialog(DIALOG_NEW_FOLDER_ID);
 //        		} else {
 //        			// if no item has been selected from the list
 //        		}
@@ -86,13 +79,76 @@ public class MoleFinderActivity extends Activity {
         	public void onClick(View v) {
 //        		if () {
 //        			// brings up the dialog box to confirm deletion
-        			deleteDialog.show();
+        			showDialog(DIALOG_DELETE_FOLDER_ID);
 //        		} else {
 //        			// if no item has been selected from the list
 //        		}
         	}
         });
+    
 
         
     }    
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+    	switch(id) {
+    	case DIALOG_NEW_FOLDER_ID:
+    		input = new EditText(MoleFinderActivity.this);
+    		Builder addDialog = new AlertDialog.Builder(MoleFinderActivity.this);
+    		addDialog.setView(input);
+    		addDialog.setTitle("Adding a new folder...")
+    		.setMessage("Please specify the folder name to add.")
+    		// do the work to define the addDialog
+    		// Setting Positive "Yes" Button
+    		.setPositiveButton("Add folder", new DialogInterface.OnClickListener() {
+    			public void onClick(DialogInterface dialog,int which) {
+    				//actions to complete when clicking yes
+    				folderName = input.getText();
+    				dialog.cancel();
+    				
+    				//just used to test the folder name
+    				Context context = getApplicationContext();
+    				int duration = Toast.LENGTH_SHORT;
+    				Toast toast = Toast.makeText(context, folderName, duration);
+    				toast.show();
+    			}
+    		})
+
+    		// Setting Negative "NO" Button
+    		.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+    			public void onClick(DialogInterface dialog, int which) {
+    				//actions to complete when clicking cancel
+    				dialog.cancel();
+    			}
+    		});
+    		return addDialog.create();
+    		//dialog.show();
+    	case DIALOG_DELETE_FOLDER_ID:
+    		Builder deleteDialog = new AlertDialog.Builder(this);
+
+    		deleteDialog.setTitle("Confirm Delete...")
+    		.setMessage("Are you sure you want to delete this group?")
+    		// do the work to define the addDialog
+    		// Setting Positive "Yes" Button
+    		.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+    			public void onClick(DialogInterface dialog,int which) {
+    				//actions to complete when clicking yes
+
+    				dialog.cancel();
+    			}
+    		})
+
+            // Setting Negative "NO" Button
+            	.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            	public void onClick(DialogInterface dialog, int which) {
+            		//actions to complete when clicking cancel
+            		dialog.cancel();
+            	}
+            });
+        	return deleteDialog.create();
+        	//deleteDialog.show();
+        }
+        return null;
+    }
 }
