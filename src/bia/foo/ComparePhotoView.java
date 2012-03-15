a520ed480fafdb36fe488889eea92e6ca74a3c40
christianjukna <jukna@ualberta.ca>
2012-03-15 15:17:34 -0600

    Revert "Added a layout and coded into the comparephotoview activity"
    This reverts commit f4d7399cf511968dcfceb9eb91d3cf2728e5eb5e.

--- src/bia/foo/ComparePhotoView.java
+++ src/bia/foo/ComparePhotoView.java
@@ -5,11 +5,10 @@ import android.os.Bundle;
 
 
 //This is the view for comparing two photos with eachother
-// this in not implemented yet
 public class ComparePhotoView extends Activity{
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.comparephotos);
+        setContentView(R.layout.main); //insert proper layout here
     }
 }
