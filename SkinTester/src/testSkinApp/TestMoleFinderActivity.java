5f1956643c3b09bdd1eb5262c224833148d950dc
christianjukna <jukna@ualberta.ca>
2012-04-04 11:40:16 -0600

    Added new test cases for folder names

--- SkinTester/src/testSkinApp/TestMoleFinderActivity.java
+++ SkinTester/src/testSkinApp/TestMoleFinderActivity.java
@@ -31,33 +31,50 @@ public class TestMoleFinderActivity extends ActivityInstrumentationTestCase2 {
 		solo = new Solo(getInstrumentation(),getActivity());
 	}
 	
-	public void testDisplayBlackBox(){
+//	public void testDisplayBlackBox(){
+//		solo.clickOnButton("Add Folder");
+//		solo.enterText(0, "Test Folder");
+//		solo.clickOnButton(0);
+//		solo.clickInList(2, 0);
+//		solo.assertCurrentActivity("Error: Photo Layout not Open", "PhotoLayoutView" );
+//		solo.goBack();
+//		solo.clickLongInList(2,0);
+//		solo.clickOnButton(0);
+//		Assert.assertFalse(solo.searchText("Test Folder"));
+//		solo.clickInList(1);
+//		solo.assertCurrentActivity("Error: Photo Layout not Open", "PhotoLayoutView");
+//		solo.clickOnScreen(50,250);
+//		solo.assertCurrentActivity("Error: Photo Layout not Open", "DisplayPhotoView" );
+//		
+//	}
+	
+//	public void testAddFolderStressTest(){
+//		int x = 0;
+//		while (x < 1000){
+//			solo.clickOnButton("Add Folder");
+//			solo.enterText(0, "Test Folder"+x);
+//			solo.clickOnButton(0);
+//			x++;
+//		}
+//	}
+	public void testFolderNames(){
 		solo.clickOnButton("Add Folder");
 		solo.enterText(0, "Test Folder");
 		solo.clickOnButton(0);
-		solo.clickInList(2, 0);
-		solo.assertCurrentActivity("Error: Photo Layout not Open", "PhotoLayoutView" );
-		solo.goBack();
-		solo.clickLongInList(2,0);
+		Assert.assertTrue(solo.searchText("Test Folder"));
+		solo.clickOnButton("Add Folder");
+		solo.enterText(0, "NEWLINE\n\n");
 		solo.clickOnButton(0);
-		Assert.assertFalse(solo.searchText("Test Folder"));
-		solo.clickInList(1);
-		solo.assertCurrentActivity("Error: Photo Layout not Open", "PhotoLayoutView");
-		solo.clickOnScreen(50,250);
-		solo.assertCurrentActivity("Error: Photo Layout not Open", "DisplayPhotoView" );
-		
+		Assert.assertFalse(solo.searchText("NEWLINE"));
+		solo.clickOnButton("Add Folder");
+		solo.enterText(0, "SINGLEQUOTE '' '' '''' '' '");
+		solo.clickOnButton(0);
+		Assert.assertFalse(solo.searchText("SINGLEQUOTE"));
+		solo.clickOnButton("Add Folder");
+		solo.enterText(0, "TOOLONG asjdhflakjsdhflkajshdlfkjahskjdhflkashjdflakjshdflkjahsdlfkjahsldkjfhalskdj");
+		solo.clickOnButton(0);
+		Assert.assertFalse(solo.searchText("TOOLONG"));
 	}
-	
-	public void testAddFolderStressTest(){
-		int x = 0;
-		while (x < 1000){
-			solo.clickOnButton("Add Folder");
-			solo.enterText(0, "Test Folder"+x);
-			solo.clickOnButton(0);
-			x++;
-		}
-	}
-	
 	@Override
 	public void tearDown() throws Exception{
 		solo.finishOpenedActivities();
